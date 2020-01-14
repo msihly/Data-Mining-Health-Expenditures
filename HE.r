e37a37a6a8dbd15d734a30b041569f78ab8c8732
msihly <43391570+msihly@users.noreply.github.com>
2020-01-13 21:09:25 -0500

    Rev. 2

--- HE.r
+++ HE.r
@@ -1,94 +1,208 @@
-# Open HealthExpend (Modified) file to read the data into R
-HE = read.csv("HealthExpend (Modified).csv", header = T, na.strings = "?")
-fix(HE)
-dim(HE)
-#
-names(HE)
-#
-# Given qualitative variables such as
-# Race, Maristat, Region, Educ, Phstat, Income, and  Indusclass,
-# R generates dummy variables automatically.
-#
-# Contrasts() function returns the coding that R uses for the dummy variables
+HE = read.csv("HealthExpend (Modified).csv")
 attach(HE)
-contrasts(RACE)
-contrasts(MARISTAT)
-contrasts(REGION)
-contrasts(EDUC)
-contrasts(PHSTAT)
-contrasts(INCOME)
-contrasts(INDUSCLASS)
-#
-# First model with EXPENDIP as DV
-# We exclude EXPENDOP from the model to perform linear regression on one DV
-lm.EXPENDIP = lm(EXPENDIP~. -EXPENDOP, data = HE)
-summary(lm.EXPENDIP)
-#
-# After running the first model, we obtain few NA values, which is a
-# sign of collinearity problem in the dataset
-#
-library(car)
-vif(lm.EXPENDIP)
-# we get an error using vif () function
-#
-# To find out which variables are highly correlated we use alias()
-alias( lm( EXPENDIP ~ ., data = HE))
-#
-# Create new model by excluding highly correlated variaables
-lm.EXPENDIP1 = update(lm.EXPENDIP, ~. - (COLLEGE +  HIGHSCH))
-summary(lm.EXPENDIP1)
-#
-# Run vif() again to check the data
-library(car)
-vif(lm.EXPENDIP1)
-#
-# We use Backward Selection Method to decide the optimal model with all the
-# variables having p-values below 0.05 or 0.1???
-#
-# INDUSCLASS has largest p-value,
-# meaning that this predictor is not statistically significant
-# Next step is to exclude INDUSCLASS variable from the model
-lm.EXPENDIP2 = update(lm.EXPENDIP1, ~. - INDUSCLASS)
-summary(lm.EXPENDIP2)
-#
-lm.EXPENDIP3 = update(lm.EXPENDIP2, ~. - RACE)
-summary(lm.EXPENDIP3)
-#
-lm.EXPENDIP4 = update(lm.EXPENDIP3, ~. - MARISTAT)
-summary(lm.EXPENDIP4)
-#
-lm.EXPENDIP5 = update(lm.EXPENDIP4, ~. - USC)
-summary(lm.EXPENDIP5)
-#
-lm.EXPENDIP6 = update(lm.EXPENDIP5, ~. - INCOME)
-summary(lm.EXPENDIP6)
-#
-lm.EXPENDIP7 = update(lm.EXPENDIP6, ~. - ANYLIMIT)
-summary(lm.EXPENDIP7)
-#
-lm.EXPENDIP8 = update(lm.EXPENDIP7, ~. - INSURE)
-summary(lm.EXPENDIP8)
-#
-lm.EXPENDIP9 = update(lm.EXPENDIP8, ~. - MANAGEDCARE)
-summary(lm.EXPENDIP9)
-#
-lm.EXPENDIP10 = update(lm.EXPENDIP9, ~. - FAMSIZE)
-summary(lm.EXPENDIP10)
-#
-lm.EXPENDIP11 = update(lm.EXPENDIP10, ~. - UNEMPLOY)
-summary(lm.EXPENDIP11)
-#
-lm.EXPENDIP12 = update(lm.EXPENDIP11, ~. - MNHPOOR)
-summary(lm.EXPENDIP12)
-#
-# F-stat is growing when insignificant predictors are removed from the model
-# R^2 is pretty low, starting with 22% and finishing at 21%
-#
-#lm.EXPENDIP13 = update(lm.EXPENDIP12, ~. - PHSTAT)
-#summary(lm.EXPENDIP13)
-#
-#lm.EXPENDIP14 = update(lm.EXPENDIP13, ~. - EDUC)
-#summary(lm.EXPENDIP14)
-#
-#lm.EXPENDIP15 = update(lm.EXPENDIP14, ~. - REGION)
-#summary(lm.EXPENDIP15)
+
+summary(HE)
+# Determine variable types
+str(HE)
+# Coerce categorical variables to factors
+factors = c("ANYLIMIT", "COLLEGE", "HIGHSCH", "GENDER", "MNHPOOR", "INSURE", "USC", "UNEMPLOY", "MANAGEDCARE")
+HE[factors] = lapply(HE[factors], factor)
+# Confirm type coercion
+str(HE)
+
+# BEGIN - Multi-linear regression of EXPENDIP
+  lm.fit = lm(EXPENDIP ~ . -EXPENDOP, data = HE)
+  summary(lm.fit)
+
+  # EDUC returning NA values--signifies collinearity; check which variables are conflicting
+  alias(lm.fit)
+
+  # Remove EDUC because of perfect collinearity with HIGHSCH and COLLEGE
+  lm.fit = update(lm.fit, ~ . -EDUC)
+  summary(lm.fit)
+
+  # BEGIN - Backwards selection
+    lm.fit = update(lm.fit, ~ . -INDUSCLASS)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -USC)
+    summary(lm.fit)
+    # Possible issues with RACE - Bin as binary on WHITE
+    lm.fit = update(lm.fit, ~ . -RACE)
+    summary(lm.fit)
+    # Possibly issues with MARISTAT - Bin as binary on MARRIED
+    lm.fit = update(lm.fit, ~ . -MARISTAT)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -ANYLIMIT)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -MANAGEDCARE)
+    summary(lm.fit)
+    # Removing INCOME, FAMSIZE, INSURE, and MNHPOOR each decrease significance
+  # END - Backwards selection
+  par(mfrow = c(2, 2))
+  plot(lm.fit)
+
+  # Observation #733 causing extreme problems based on quad-plot
+  HE[733,]
+
+  # Remove outlier
+  HE = HE[-733,]
+
+  # Re-fit model to modified dataset
+  lm.fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC -INDUSCLASS -USC -RACE -MARISTAT -ANYLIMIT -MANAGEDCARE, data = HE)
+  summary(lm.fit)
+
+  # Model has changed drastically; must recreate model
+  lm.fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE)
+  summary(lm.fit)
+
+  # BEGIN - Backwards selection
+    lm.fit = update(lm.fit, ~ . -HIGHSCH)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -INSURE)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -FAMSIZE)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -INDUSCLASS)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -UNEMPLOY)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -USC)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . -RACE)
+    summary(lm.fit)
+    # Removing RACE surprisingly increases ADJR2
+    lm.fit = update(lm.fit, ~ . -MNHPOOR)
+    summary(lm.fit)
+    # Removing MNHPOOR has negligble positive effect on p-values only
+    # Removing ANYLIMIT, COLLEGE, MANAGEDCARE, MARISTAT, PHSTAT, INCOME, or REGION lower ADJR2 at this point
+  # END
+
+  # Optimal model reached with backwards selection:
+  lm.fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC -HIGHSCH -INSURE -FAMSIZE -INDUSCLASS -UNEMPLOY -USC -RACE -MNHPOOR, data = HE)
+  summary(lm.fit)
+
+  # BEGIN - Interaction terms
+    lm.fit = update(lm.fit, ~ . +MARISTAT:INCOME)
+    summary(lm.fit)
+    lm.fit = update(lm.fit, ~ . +PHSTAT:INCOME)
+    summary(lm.fit)
+    # PHSTAT:MARISTAT has a negligible increase
+    # REGION:INCOME, REGION:COLLEGE, AGE:INCOME, AGE:GENDER, MARISTAT:GENDER, ANYLIMIT:MANAGEDCARE each decrease ADJR2
+  # END
+
+  # Optimal model reached with interaction terms:
+    lm.fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC -HIGHSCH -INSURE -FAMSIZE -INDUSCLASS -UNEMPLOY -USC -RACE -MNHPOOR +MARISTAT:INCOME +PHSTAT:INCOME, data = HE)
+    summary(lm.fit)
+
+# END
+
+# BEGIN - Model selection using stepAIC
+  library(MASS)
+
+  fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE)
+  step = stepAIC(fit, direction = "backward")
+  step$anova
+
+  fit = lm(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE)
+  step = stepAIC(fit, direction = "both")
+  step$anova
+
+  # Optimal model achieved in both backward-stepwise and both-stepwise:
+  fit = lm(EXPENDIP ~ AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT, data = HE)
+  summary(fit)
+
+  # Refitting the original model with interaction terms causes excessively long processing time in R due to
+  # single-core restrictions. Parallel processing not recommended / infeasible for stepwise selection. Instead,
+  # interaction terms are fitted to the optimal base model achieved above.
+  fit = lm(EXPENDIP ~ (AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT)^2, data = HE)
+  step = stepAIC(fit, direction = "backward")
+  # Processing takes less than two seconds to find optimal model with interaction terms
+  step$anova
+
+  sum = summary(fit)
+  sum$adj.r.squared
+  sum$fstatistic
+  sum$sigma
+  # Significant increase in ADJR2
+
+  # StepAIC with direction = "both"
+  fit = lm(EXPENDIP ~ (AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT)^2, data = HE)
+  step = stepAIC(fit, direction = "both")
+  step$anova
+
+  sum = summary(fit)
+  sum$adj.r.squared
+  sum$fstatistic
+  sum$sigma
+  # Results are identical to backward-stepwise model with interaction terms
+
+  # Optimal model achieved through two-term interaction stepAIC in both backward and both directions:
+  fit = lm(EXPENDIP ~ AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT + AGE:GENDER + AGE:COUNTIP +
+             AGE:COUNTOP + GENDER:COUNTIP + COUNTIP:COUNTOP + COUNTIP:INCOME + COUNTIP:PHSTAT +
+             COUNTOP:INCOME + COUNTOP:PHSTAT + INCOME:PHSTAT, data = HE)
+  summary(fit)
+
+  # Fit model with three-term interactions
+  fit = lm(EXPENDIP ~ (AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT)^3, data = HE)
+  step = stepAIC(fit, direction = "both")
+  step$anova
+
+  sum = summary(fit)
+  sum$adj.r.squared
+  sum$fstatistic
+  sum$sigma
+  # ADJR2 significantly increased over two-term interaction model. However, model includes too many interaction
+  # terms to feasibly interpret. RSE has also decreased, but so have the degrees of freedom. Model may also be
+  # overfitted and F-Stat has decreased (as well as the DF).
+
+  # Optimal model achieved through three-term interaction stepAIC on both directions:
+  fit = lm(EXPENDIP ~ AGE + GENDER + COUNTIP + COUNTOP + INCOME + PHSTAT +
+             AGE:GENDER + AGE:COUNTIP + AGE:COUNTOP + AGE:INCOME + AGE:PHSTAT +
+             GENDER:COUNTIP + GENDER:COUNTOP + GENDER:INCOME + GENDER:PHSTAT +
+             COUNTIP:COUNTOP + COUNTIP:INCOME + COUNTIP:PHSTAT + COUNTOP:INCOME +
+             COUNTOP:PHSTAT + INCOME:PHSTAT + AGE:GENDER:COUNTIP + AGE:GENDER:COUNTOP +
+             AGE:COUNTIP:COUNTOP + AGE:COUNTIP:INCOME + AGE:COUNTIP:PHSTAT +
+             AGE:COUNTOP:INCOME + AGE:COUNTOP:PHSTAT + GENDER:COUNTIP:COUNTOP +
+             GENDER:COUNTIP:INCOME + GENDER:COUNTIP:PHSTAT + GENDER:COUNTOP:INCOME +
+             GENDER:COUNTOP:PHSTAT + COUNTIP:COUNTOP:INCOME + COUNTIP:COUNTOP:PHSTAT +
+             COUNTIP:INCOME:PHSTAT + COUNTOP:INCOME:PHSTAT)
+  summary(fit)
+
+# END
+
+# BEGIN - Failed attempt at using regsubsets of "leaps" package for finding optimal regression model
+  # install.packages("leaps")
+  # library(leaps)
+  #
+  # reg.fit = regsubsets(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE)
+  # reg.sum = summary(reg.fit)
+  #
+  # reg.fit.max = regsubsets(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE, nvmax = 22)
+  # reg.sum.max = summary(reg.fit.max)
+  #
+  # par(mfrow = c(2, 2))
+  # plot(reg.sum$rss, xlab = "Number of Variables", ylab = "RSS", type = "l")
+  # plot(reg.sum$adjr2, xlab = "Number of Variables", ylab = "Adjusted R-Sq", type = "l")
+  # max = which.max(reg.sum$adjr2)
+  # points(max, reg.sum$adjr2[max], col = "red", cex = 2, pch = 20)
+  #
+  # plot(reg.sum.max$rss, xlab = "Number of Variables", ylab = "RSS", type = "l")
+  # plot(reg.sum.max$adjr2, xlab = "Number of Variables", ylab = "Adjusted R-Sq", type = "l")
+  #
+  # plot(reg.fit.max, scale = "r2")
+  # plot(reg.fit.max, scale = "adjr2")
+  # plot(reg.fit.max, scale = "Cp")
+  # plot(reg.fit.max, scale = "bic")
+  #
+  # reg.fit.bwd = regsubsets(EXPENDIP ~ . -EXPENDOP -EDUC, data = HE, nvmax = 22, method = "backward")
+  # summary(reg.fit.bwd)
+# END
+
+# pairs(HE, upper.panel = NULL, pch = 19, main = "Scatterplot Matrix of 'HE'", row1attop = F, lower.panel = panel.smooth)
+
+# BEGIN - Hierarchical Clustering - doesn't work
+  # HE1 = HE[c("AGE", "FAMSIZE", "COUNTIP", "EXPENDIP", "COUNTOP", "EXPENDOP")]
+  # HE1.scl = scale(HE1, center = F)
+  # hclus.comp = hclust(dist(HE1.scl), method = "average")
+  # plot(hclus.comp, main = "Complete Linkage", xlab = "", sub = "", cex = .9)
+# END
